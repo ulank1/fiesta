71774598c5180a68c3e0ad75c7325a67ce1a72bc
ulank1 <ulankarimov@mail.ru>
2016-08-27 18:02:45 +0600

    first commit

---
+++ post/api.py
@@ -0,0 +1,14 @@
+from tastypie.authorization import Authorization
+from tastypie.resources import ModelResource, ALL, ALL_WITH_RELATIONS
+from .models import Post
+
+
+class PostResource(ModelResource):
+    # user = fields.ForeignKey(UserResource, 'user')
+    class Meta:
+        limit = 0
+        max_limit = 0
+        queryset = Post.objects.all()
+        resource_name = 'post'
+        allowed_methods = ['get', 'post', 'put', 'delete']
+        authorization = Authorization()
